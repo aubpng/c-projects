3967e2f78a86aa92681e616b3ab28e6774c7fba4
aubpng <123448144+aubpng@users.noreply.github.com>
2023-02-25 22:08:45 +1100

    Added 3 more operations

--- calculator/main.c
+++ calculator/main.c
@@ -2,9 +2,35 @@
 // Calculator written in C for basic arithmetic using floats
 
 int main() {
-    float numOne, numTwo;
+    
+    float numOne, numTwo, result;
+    char optn;
+
+    // More operators shall be added soon
+    printf("Enter an operation (+ - * /): ");
+    scanf("%c", &optn);
     printf("Enter two numbers: ");
     scanf("%f %f", &numOne, &numTwo);
-    printf("%.2f + %.2f = %.2f\n", numOne, numTwo, numOne + numTwo);
+    
+    switch (optn) {
+        case '+':
+            result = numOne + numTwo;
+            printf("%.2f\n", result);
+            break;
+        case '-':
+            result = numOne - numTwo;
+            printf("%.2f\n", result);
+            break;
+        case '*':
+            result = numOne * numTwo;
+            printf("%.2f\n", result);
+            break;
+        case '/':
+            result = numOne / numTwo;
+            printf("%.2f\n", result);
+            break;
+        default:
+            printf("%c is invalid", optn);
+    }
     return 0;
 }
