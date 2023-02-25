c703044f0ccd3626555435d663d049be84fbb94f
aubpng <123448144+aubpng@users.noreply.github.com>
2023-02-25 23:01:54 +1100

    Added converter

---
+++ converter/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+// Measurement converter
+
+int main() {
+    // Just cm to mm for now
+    float cm, mm;
+    printf("Enter measurement in cm: ");
+    scanf("%f", &cm);
+    printf("%.2f cm = %.2f mm\n", cm, cm * 10);
+    return 0;
+}
